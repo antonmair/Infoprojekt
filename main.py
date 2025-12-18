eeca54807d4f11e04be7fcfc81c090439e92fdfe
antonmair <maan1052@h-ka.de>
2025-12-18 19:34:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ class Request:
 
 
   
-    def calc_co2(self, distance):  
+    def something(self, ):  
 
   
-def main():
-  
-  from angle import 
+def main():  
+    p1 = input("Point 1:")
+    p2 = input("Point 2:")
+    p3 = input("Point 3:")
+    p4 = input("Point 4:")
+    polist = [p1,p2,p3,p4]
+    from angle import anglescript
+    if __name__ == '__main__':
+        anglescript()
+
