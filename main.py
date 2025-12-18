a7ef3d8b1a34d84d2746e84dc70acba0c061fab0
antonmair <maan1052@h-ka.de>
2025-12-18 21:43:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,14 +19,34 @@ def main():
         [y1,y2,y3,y4]
     ]
     #polist range on first iteration is from [0][0] and [1][3]
+    #create request as class (not sure why)
     request = Request(polist)
-    #calculate angles
-    from angle_calc import anglcalc
-    if __name__ == '__main__':
-        anglcalc()
+
+    for(){# amount of iterations (line versions)
         
-    #calculate punishment
-    from angle_punishment import anglescript
-    if __name__ == '__main__':
-        anglescript()
+        #calculate angles
+        from angle_calc import anglcalc
+        if __name__ == '__main__':
+            anglcalc(polist)#to check: does anglcalc need polist and why and when (is correct usage of anglcalc(polist)) same for the others
+        #returns anglearr
+        
+        #calculate punishment
+        from angle_punishment import anglescript
+        if __name__ == '__main__':
+            anglescript(anglearr)
+        #returns penalty
+        
+        #insert points
+        from pointinserter import spawner
+        if __name__ == '__main__':
+            spawner(polist)#spawns points between every x and y point in array at half way but not betwen first and second, and penult and ult
+        #returns polistnew 
+        polist = polistnew
+        #returns newpoints (all points spawned this iteration)
+
+        for(){#amount of newpoint ( len(newpoints)
+            from algorithm import pointmover
+            if __name__ == '__main__':
+                pointmover(#newpoint x and y)
+
 
