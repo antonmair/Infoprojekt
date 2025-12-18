bb25453edc999ea9999e94e1ba683bf27cccdc76
antonmair <maan1052@h-ka.de>
2025-12-18 20:57:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 #solve Trassenfinder
 class Request:
-    def __init__():##unfinished
-        self.whatever = whatever
-
-
-  
-    def something(self, ):  
+    def __init__(self, polist):##unfinished
+        self.polist = polist
 
   
 def main():  
+    request = Request(polist)
     x1 = input("x 1:")
     x2 = input("x 2:")
     x3 = input("x 3:")
@@ -19,7 +16,10 @@ def main():
     y4 = input("y 4:")
     polist = [x1,x2,x3,x4],
     [y1,y2,y3,y4]
-    from angle import anglescript
+    from angle_calc import anglcalc
+    if __name__ == '__main__':
+        anglcalc()
+    from angle_punishment import anglescript
     if __name__ == '__main__':
         anglescript()
 
