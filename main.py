e1c3cf40a61a369e1dd4a3182a66c7014160841a
antonmair <maan1052@h-ka.de>
2025-12-18 19:21:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #solve Trassenfinder
+class Request:
+    def __init__():##unfinished
+        self.whatever = whatever
+
+
+  
+    def calc_co2(self, distance):  
+
+  
+def main():
+  
+  from angle import 
