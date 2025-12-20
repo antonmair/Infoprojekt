5a0f55f16f7605abfaf591a3a3989e446d7e03b5
antonmair <maan1052@h-ka.de>
2025-12-20 18:20:07 +0100

    Update main.py
    replaced main.py with everything currently not working commented

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ class Request:
     def __init__(self, polist):##unfinished
         self.polist = polist
 
-  
+
 def main():  
     #Create pointlist (polist) using user imput
-    #x1 = input("x 1:")#start of vec1 x
-    #x2 = input("x 2:")#end of vec1 x 
-    #x3 = input("x 3:")#end of vec2 x
-    #x4 = input("x 4:")#start of vec2 x
-    #y1 = input("y 1:")#start of vec1 y
-    #y2 = input("y 2:")#end of vec 1 y 
-    #y3 = input("y 3:")#end of vec 2 y
-    #y4 = input("y 4:")#start of vec 2 y
-
+    # x1 = input("x 1:")#start of vec1 x
+    # x2 = input("x 2:")#end of vec1 x 
+    # x3 = input("x 3:")#end of vec2 x
+    # x4 = input("x 4:")#start of vec2 x
+    # y1 = input("y 1:")#start of vec1 y
+    # y2 = input("y 2:")#end of vec 1 y 
+    # y3 = input("y 3:")#end of vec 2 y
+    # y4 = input("y 4:")#start of vec 2 y
+    
     #dev version
     x1=1
     x2=2
@@ -24,7 +24,6 @@ def main():
     y2=6
     y3=7
     y4=8
-    
     polist = [
         [x1,x2,x3,x4],
         [y1,y2,y3,y4]
@@ -34,45 +33,50 @@ def main():
     #create request as class (not sure why)
     request = Request(polist)
 
-    for i in range(5):# amount of iterations (line versions)
+    for i in range(1):# amount of iterations (line versions)
         
         #plot polist
         from plotter import polistplot
         if __name__ == '__main__':
-            polistplost(polist)
+            polistplot(polist)
+        # #calculate angles
+        # from angle_calc import anglcalc
+        # if __name__ == '__main__':
+        #     anglcalc(polist)#to check: does anglcalc need polist and why and when (is correct usage of anglcalc(polist)) same for the others
+        # #returns anglearr
         
-        #calculate angles
-        from angle_calc import anglcalc
-        if __name__ == '__main__':
-            anglcalc(polist)#to check: does anglcalc need polist and why and when (is correct usage of anglcalc(polist)) same for the others
-        #returns anglearr
-        
-        #calculate punishment
-        from angle_punishment import anglescript
-        if __name__ == '__main__':
-            anglescript(anglearr)
-        #returns penalty
+        # #calculate punishment
+        # from angle_punishment import anglescript
+        # if __name__ == '__main__':
+        #     anglescript(anglearr)
+        # #returns penalty
         
         #insert points
         from pointinserter import spawner
         if __name__ == '__main__':
-            spawner(polist)#spawns points between every x and y point in array at half way but not betwen first and second, and penult and ult
+            #spawner(polist)
+            polistnew = spawner(polist)
+            # from pointinserter import polistnew
+            # print(polistnew)
+
+            #spawns points between every x and y point in array at half way but not betwen first and second, and penult and ult
         #returns polistnew 
         #returns newpoints (all points spawned this iteration)
 
-        for j in range(len(newpoints)):#amount of newpoint ( len(newpoints)
+        # for j in range(len(newpoints)):#amount of newpoint ( len(newpoints)
             
-            #moves one point
-            from algorithm import pointmover
-            if __name__ == '__main__':
-                pointmover(polistnew[0][i],polistnew[1][i]) #function pointmover calls anglcalc and aglescript seperatly within the algorithm.py file
-                #returns newx, newy (x and y of best newpoint)
-                polistnew[0][i] = newx
-                polistnew[1][i] = newy
-        }
+        #     #moves one point
+        #     from algorithm import pointmover
+        #     if __name__ == '__main__':
+        #         pointmover(polistnew[0][i],polistnew[1][i]) #function pointmover calls anglcalc and aglescript seperatly within the algorithm.py file
+        #         #returns newx, newy (x and y of best newpoint)
+        #         polistnew[0][i] = newx
+        #         polistnew[1][i] = newy
+        
 
-        #end of iteration finalize changes into polist
+        # #end of iteration finalize changes into polist
         polist = polistnew
-    }
+        print("printdoch")
+        print(polist)
 
-
+main()
