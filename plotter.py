7827248f5e9cf27700ba098538be49e1e211fdc1
antonmair <maan1052@h-ka.de>
2025-12-20 17:19:23 +0100

    Update plotter.py
    working version

--- plotter.py
+++ plotter.py
@@ -1,22 +1,33 @@
 import matplotlib.pyplot as plt
 def polistplot(polist):
-    #print(polist[0][1])
-    endx = int(polist[0][1])
-    startx =  int(polist[0][0])
-    endy = int(polist[1][1])
-    starty = int(polist[1][0])
 
-    # deltax = polist[0][1] - polist[0][0]
-    # deltay = polist[1][1] - polist[1][0]
-    deltax = endx - startx
-    deltay = endy - starty
-    print(startx)
-    print(starty)
-    plt.arrow(1, 5, 1, 2, width=0.05)
-    plt.arrow(startx, starty, deltax, deltay, width=0.05)
-    # del polist[0][0]
-    # del polist[1][0]
-    # del polist[0][len(polist)]
-    # del polist[1][len(polist)]
+    #vector 1
+    endx1 = int(polist[0][1])
+    startx1 =  int(polist[0][0])
+    endy1 = int(polist[1][1])
+    starty1 = int(polist[1][0])
+
+    #vector 2
+    endx2 = int(polist[0][len(polist)])
+    startx2 =  int(polist[0][len(polist)+1])
+    endy2 = int(polist[1][len(polist)])
+    starty2 = int(polist[1][len(polist)+1])    
+
+    #steigung v1
+    deltax1 = endx1 - startx1
+    deltay1 = endy1 - starty1
+
+    #steigung v2
+    deltax2 = endx2 - startx2
+    deltay2 = endy2 - starty2
+
+    plt.arrow(startx1, starty1, deltax1, deltay1, width=0.05)
+    plt.arrow(startx2, starty2, deltax2, deltay2, width=0.05)
+
     plt.plot(polist[0],polist[1])
     plt.show()
+
+    #final dynamic show version
+    # plt.ioff()
+    # plt.gcf().show()
+    #plt.pause(5)
