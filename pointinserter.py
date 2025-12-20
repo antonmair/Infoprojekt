4beb2a7f43b43de660950e0c0b43eed4e0145fe3
antonmair <maan1052@h-ka.de>
2025-12-20 18:20:48 +0100

    Update pointinserter.py

--- pointinserter.py
+++ pointinserter.py
@@ -1,1 +1,8 @@
-
+def spawner(polist):
+    for i in range(len(polist)):
+            #why just one iteration??????
+        #if polist[0][i]!=polist[0][0] & polist[0][i]!=polist[len(polist[0])] & polist[0][i]!=polist[len(polist[0]-1)]:
+            polist.insert(i,((polist[0][i]+polist[0][i+1])/2, (polist[1][i]+polist[1][i+1])/2))
+            #polistnew = polist
+            print(polist)
+            return polist
