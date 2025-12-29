47658e70d9a3f5446d78c3739c945e56ed37dbdd
antonmair <maan1052@h-ka.de>
2025-12-29 17:49:15 +0100

    Update _main.py
    reintroduced dev version i forgot

--- _main.py
+++ _main.py
@@ -6,14 +6,56 @@ class Request:
 
 def main():  
     #Create pointlist (polist) using user imput
-    x1 = input("x 1:")#start of vec1 x
-    x2 = input("x 2:")#end of vec1 x 
-    x3 = input("x 3:")#end of vec2 x
-    x4 = input("x 4:")#start of vec2 x
-    y1 = input("y 1:")#start of vec1 y
-    y2 = input("y 2:")#end of vec 1 y 
-    y3 = input("y 3:")#end of vec 2 y
-    y4 = input("y 4:")#start of vec 2 y
+    # x1 = input("x 1:")#start of vec1 x
+    # x2 = input("x 2:")#end of vec1 x 
+    # x3 = input("x 3:")#end of vec2 x
+    # x4 = input("x 4:")#start of vec2 x
+    # y1 = input("y 1:")#start of vec1 y
+    # y2 = input("y 2:")#end of vec 1 y 
+    # y3 = input("y 3:")#end of vec 2 y
+    # y4 = input("y 4:")#start of vec 2 y
+    
+    #dev version
+
+    #this example is a bit off
+    # x1=2
+    # x2=2.1
+    # x3=5
+    # x4=6
+    # y1=1
+    # y2=2
+    # y3=5
+    # y4=5
+
+    #this example works perfectly, i think
+    # x1=1
+    # x2=2
+    # x3=20
+    # x4=21
+    # y1=5
+    # y2=6
+    # y3=21
+    # y4=22
+
+    #this one doesnt work
+    # x1=1
+    # x2=2
+    # x3=3
+    # x4=4
+    # y1=5
+    # y2=6
+    # y3=7
+    # y4=8
+
+    #example with multiple same works perfectly until i=4
+    x1=1
+    x2=2
+    x3=8
+    x4=9
+    y1=3
+    y2=3
+    y3=3
+    y4=3
     polist = [
         [x1,x2,x3,x4],
         [y1,y2,y3,y4]
