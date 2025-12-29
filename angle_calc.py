173356ab6990dbeb61c861a4abb484cf1323f4cb
antonmair <maan1052@h-ka.de>
2025-12-29 17:17:44 +0100

    Update angle_calc.py

--- angle_calc.py
+++ angle_calc.py
@@ -1,1 +1,7 @@
-
+#requierements for angle_calc:
+#gets p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y and calculated angle at p2,p3,p4, no need for full polist calculation
+#function is called anglcalc
+#and is called with given variables anglcalc(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y)
+#returns list of [angle, angle, angle] fixed length at 3
+def anglcalc(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y):
+    return 0
