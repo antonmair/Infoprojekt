cb594ab3b22ba732c6c2d1ff2b1abc9d218ae58a
antonmair <maan1052@h-ka.de>
2025-12-31 01:25:45 +0100

    Update plotter.py
    removed redundand code

--- plotter.py
+++ plotter.py
@@ -2,11 +2,6 @@ import matplotlib.pyplot as plt
 def polistplot(polist):
     plt.plot(polist[0],polist[1])
     plt.scatter(polist[0],polist[1])
-    #dumb show
-    #plt.show().set_aspect('equal')
-    #plt.gca().set_aspect('equal')
-
-    final dynamic show version
     plt.ioff()
     plt.gcf().show()
     plt.gca().set_aspect('equal')
