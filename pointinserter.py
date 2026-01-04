3e4d2888415b6e0903f4b3e3b0e0877f20e85813
antonmair <maan1052@h-ka.de>
2026-01-04 01:53:23 +0100

    new pointinserter with punshiement

--- pointinserter.py
+++ pointinserter.py
@@ -1,15 +1,21 @@
 from angle_calc import anglcalc
-from angle_punishment import anglescript
-
-def spawner(polist):
+def spawner(polist, itera):
     rangesetter = len(polist[0])-1 #rangsetter is amount of points and thus amount of iterations
     i=0
+    isfirst = False
+    islast = False
     while i < rangesetter:
         if(i != 0 and i != rangesetter-1 and i != rangesetter): #check if point has to be created (dont create point if in start vectors)
-
+            if i == 1:
+                isfirst = True
+            if i == rangesetter-2:
+                islast = True
             #define points to work with for current point
             p1x = float(polist[0][i-1])
             p1y = float(polist[1][i-1])
+            #if i> 1: #funky alternative version that overcorrects
+            #    p1x = float(polist[0][i-2])
+            #    p1y = float(polist[1][i-2])
             p2x = float(polist[0][i])
             p2y = float(polist[1][i])
             p3x = ((polist[0][i]+polist[0][i+1])/2) #this defines the new current points x
@@ -24,11 +30,11 @@ def spawner(polist):
             deltay = p4y - p2y
 
             #general setup for pointmover
-            spacing = 0.05 #sets search spacing
+            spacing = 0.0001 #sets search spacing
             finalx = p3x
             finaly = p3y
-            punishment = anglescript(anglcalc(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y)) #calculates punishments with given points
-
+            punishment = anglcalc(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y, itera, isfirst, islast) #calculates punishments with given points
+            #print("importatn", punishment)
             #first loop inverting deltax (search left of line)
             j=0.0 #iterator
 
@@ -42,7 +48,9 @@ def spawner(polist):
                 p3x = p3x+spacing*deltax
                 p3y = p3y+spacing*deltay
                 j=j+spacing
-                newcalcpun = anglescript(anglcalc(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y)) #calculates punishments with given points
+                #newcalcpun = anglescript(anglcalc(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y)) #calculates punishments with given points
+                newcalcpun = anglcalc(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y, itera, isfirst, islast)
+                #print("newcalcpun iteration a", newcalcpun)
                 if  newcalcpun <= punishment: #if better solution found insert as preliminary finalist #using <= only in test version remove later
                     finalx = p3x
                     finaly = p3y
@@ -63,7 +71,9 @@ def spawner(polist):
                 p3x = p3x+spacing*deltax
                 p3y = p3y+spacing*deltay
                 j=j+spacing
-                newcalcpun = anglescript(anglcalc(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y))
+                #newcalcpun = anglescript(anglcalc(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y))
+                newcalcpun = anglcalc(p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y,p5x,p5y, itera, isfirst, islast)
+                #print("newcalcpun iteration a", newcalcpun)
                 if  newcalcpun <= punishment: #if better solution found insert as preliminary finalist #using <= only in test version remove later
                     finalx = p3x
                     finaly = p3y
